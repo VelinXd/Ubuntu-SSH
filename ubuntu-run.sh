148f4bcacba7012f37b80aaaf63b92869ed9aa31
HeyYoWhatBro <69439251+HeyYoWhatBro@users.noreply.github.com>
2023-07-22 17:43:16 +0800

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/HeyYoWhatBro/kernel_xiaomi_mt6768 -b Atomic-A11 kernel
+git clone https://github.com/HeyYoWhatBro/kernel_xiaomi_mt6768 -b Atomic-A13 kernel
 cd kernel
 echo "
 # Telegram Chat ID to send build info
