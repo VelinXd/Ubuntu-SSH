5968dbcfd3d93ea92e6043300045a5e22318e2d1
HeyYoWhatBro <69439251+HeyYoWhatBro@users.noreply.github.com>
2023-07-22 11:01:09 +0800

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,6 +1,6 @@
 gh=${{ env.GH_NAME }}
 echo $GH_NAME
-git clone https://${{env.GH_NAME}}:${{env.GH_PASS}}@github.com/HeyYoWhatBro/kernel_xiaomi_mt6768 -b Atomic-A11 kernel
+git clone https://${{env.GH_NAME}}:${{env.GH_PASS}}@github.com/kernel_xiaomi_mt6768 -b Atomic-A11 kernel
 cd kernel
 echo "
 # Telegram Chat ID to send build info
