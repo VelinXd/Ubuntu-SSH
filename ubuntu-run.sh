32b306538a8e00649284b203f5c487815a459101
nuuwy0 <69439251+nuuwy0@users.noreply.github.com>
2023-08-06 20:20:50 +0800

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,5 +1,5 @@
-git clone https://$(env.GH_PASS)@github.com/nuuwy0/kernel_xiaomi_mt6768 kernel
-cd kernel
+#git clone https://$(env.GH_PASS)@github.com/nuuwy0/kernel_xiaomi_mt6768 kernel
+#cd kernel
 echo "
 # Telegram Chat ID to send build info
 TELEGRAM_CHAT="-1001769232453"
@@ -13,4 +13,4 @@ ClangName="azure"
 # Cleanup after successful compile
 CLEANUP="yes"
 " > config.env
-bash kb.sh
+#bash kb.sh
