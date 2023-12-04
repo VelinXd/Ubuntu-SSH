6f585b8c1f0f1c2b1cef14cc8e9f9155102d3445
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-04 21:40:36 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -9,15 +9,15 @@ sleep 5s
 git clone --depth=1 https://github.com/eraselk/realme_C11_C12_C15_kernel kernel
 cd kernel
 sleep 5s
-git clone --depth=1 https://github.com/Sukrut4778/gcc-linaro-7.4.1 gcc64
-git clone --depth=1 https://github.com/Sukrut4778/gcc-linaro-7.4.1-32 gcc32
+#git clone --depth=1 https://github.com/Sukrut4778/gcc-linaro-7.4.1 gcc64
+#git clone --depth=1 https://github.com/Sukrut4778/gcc-linaro-7.4.1-32 gcc32
 touch build.sh 
 echo "export KBUILD_BUILD_USER="eraselk"
 export LOCALVERSION="codespace"
 make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
 make -j$(nproc --all) ARCH=arm64 O=out \
-CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-gnu-" \
-CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-gnueabi-"
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz-dtb" >> build.sh
 # KSU
 if [[ $KSU == "1" ]]; then
