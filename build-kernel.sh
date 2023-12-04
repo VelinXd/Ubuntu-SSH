0522b889c96f3f72086ec121d62e11b25ea40ec0
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-04 22:06:06 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -16,8 +16,8 @@ echo "export KBUILD_BUILD_USER="eraselk"
 export LOCALVERSION="codespace"
 make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
 make -j$(nproc --all) ARCH=arm64 O=out \
-CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-gnu-" \
-CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-gnueabi-"
+CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-elf-" \
+CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-none-eabi-"
 curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz-dtb" >> build.sh
 # KSU
 if [[ $KSU == "1" ]]; then
