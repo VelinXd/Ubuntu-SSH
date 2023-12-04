97018424ecbe1ad94c096ccb60e68f370a527929
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-04 20:02:52 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,4 +1,19 @@
 #! /usr/bin/bash
+# NON KSU
+# SCRIPT BY ERASELK
+
+
+# cleaning
+clean() {
+if [ -d "out" ]; then
+rm -rf out
+sudo make clean
+clone
+else
+clone
+fi
+}
+clone() {
 sudo apt-get update -y && sudo apt-get upgrade -y ; sudo apt-get install python2 -y
 sleep 5s
 git clone --depth=1 https://github.com/eraselk/realme_C11_C12_C15_kernel kernel
@@ -12,4 +27,9 @@ make -j$(nproc --all) ARCH=arm64 O=out \
 CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-gnu-" \
 CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-gnueabi-"
 curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz-dtb" >> build.sh
+}
+build() {
 find . -type f -exec chmod 777 {} + ; bash build.sh
+}
+clone 
+build
