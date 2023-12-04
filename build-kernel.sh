02177625b6b38eb1b8217d65dbf77963a624259c
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-04 20:20:32 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/bash
 # NON KSU
 # SCRIPT BY ERASELK
-KERNEL_SOURCE=
-DEFCONFIG=
+KERNEL_SOURCE=   
+DEFCONFIG=oppo6765_defconfig         
+KSU=1        
 ARCH=arm64
 
 # cleaning
 clean() {
-if [ -d "out" ]; then
+if [[ -d "out" ]]; then
 rm -rf out
 sudo make clean
 clone
@@ -20,7 +21,11 @@ sudo apt-get update -y && sudo apt-get upgrade -y ; sudo apt-get install python2
 sleep 5s
 git clone --depth=1 ${KERNEL_SOURCE} kernel
 cd kernel
+sleep 5s
+
+# GCC
 git clone --depth=1 https://github.com/EternalX-project/aarch64-linux-gnu gcc64 ; git clone --depth=1 https://github.com/EternalX-project/arm-linux-gnueabi.git gcc32
+
 touch build.sh 
 echo "export KBUILD_BUILD_USER="eraselk"
 export LOCALVERSION="codespace
@@ -29,6 +34,17 @@ make -j$(nproc --all) ${ARCH} O=out \
 CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-gnu-" \
 CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-gnueabi-"
 curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz-dtb" >> build.sh
+# KSU
+if [[ $KSU == "1" ]]; then
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+        echo "CONFIG_KPROBES=y" >> arch/${ARCH}/configs/${DEFCONFIG}
+        echo "CONFIG_HAVE_KPROBES=y" >> arch/${ARCH}/configs/${DEFCONFIG}
+        echo "CONFIG_KPROBE_EVENTS=y" >> arch/${ARCH}/configs/${DEFCONFIG}
+        echo "CONFIG_OVERLAY_FS=y" >> arch/${ARCH}/configs/${DEFCONFIG}
+fi
+if [[ $KSU == "0" ]]; then
+build
+fi
 }
 build() {
 find . -type f -exec chmod 777 {} + ; bash build.sh
