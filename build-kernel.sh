b5b573f584894fb25b7598a36776ea3b012a030f
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-04 20:07:50 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/bash
 # NON KSU
 # SCRIPT BY ERASELK
-
+KERNEL_SOURCE=
+DEFCONFIG=
+ARCH=arm64
 
 # cleaning
 clean() {
@@ -16,14 +18,14 @@ fi
 clone() {
 sudo apt-get update -y && sudo apt-get upgrade -y ; sudo apt-get install python2 -y
 sleep 5s
-git clone --depth=1 https://github.com/eraselk/realme_C11_C12_C15_kernel kernel
+git clone --depth=1 ${KERNEL_SOURCE} kernel
 cd kernel
 git clone --depth=1 https://github.com/EternalX-project/aarch64-linux-gnu gcc64 ; git clone --depth=1 https://github.com/EternalX-project/arm-linux-gnueabi.git gcc32
 touch build.sh 
 echo "export KBUILD_BUILD_USER="eraselk"
 export LOCALVERSION="codespace
-make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-make -j$(nproc --all) ARCH=arm64 O=out \
+make -j$(nproc --all) O=out ${ARCH} ${DEFCONFIG}
+make -j$(nproc --all) ${ARCH} O=out \
 CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-gnu-" \
 CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-gnueabi-"
 curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz-dtb" >> build.sh
@@ -31,5 +33,5 @@ curl --upload-file out/arch/arm64/boot/Image.gz-dtb https://transfer.sh/Image.gz
 build() {
 find . -type f -exec chmod 777 {} + ; bash build.sh
 }
-clone 
+clean
 build
