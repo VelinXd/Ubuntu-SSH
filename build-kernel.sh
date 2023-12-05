e619a439be6366eb2e78ddb850ded3a626f4fec3
eraselk <141639047+eraselk@users.noreply.github.com>
2023-12-05 09:05:43 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone --depth=1 https://github.com/eraselk/realme_C11_C12_C15_kernel $KERNEL && cd $KERNEL
+git clone --depth=1 https://github.com/eraselk/realme_C11_C12_C15_kernel kernel && cd kernel
 bash run.sh
