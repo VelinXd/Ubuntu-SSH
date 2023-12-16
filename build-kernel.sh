b10f676ef4e695e5e46a08edf72719e9dfe685e8
VelinXd <153659670+VelinXd@users.noreply.github.com>
2023-12-17 01:03:08 +0700

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TEST=1
 if [[ $TEST == "0" ]]; then
-git clone --depth=1 https://github.com/VelinXd/kernel_realme_mt6765Q kernel && cd kernel
+git clone --depth=1 https://github.com/VelinXd/kernel_xiaomi_bengal_515 kernel && cd kernel
 bash run.sh
 else
-git clone --depth=1 https://github.com/VelinXd/kernel_realme_mt6765Q -b main kernel && cd kernel
+git clone --depth=1 https://github.com/VelinXd/kernel_xiaomi_bengal_515 -b main kernel && cd kernel
 bash run.sh
 fi
