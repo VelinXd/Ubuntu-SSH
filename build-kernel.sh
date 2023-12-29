b7d5ed57d59e1475d5272b9da5df4e45ae22e991
VelinXd <153659670+VelinXd@users.noreply.github.com>
2023-12-30 03:35:11 +0700

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TEST=1
 if [[ $TEST == "0" ]]; then
-git clone --depth=1 https://github.com/VelinXd/kernel_realme_mt6765Q kernel && cd kernel
+git clone --depth=1 https://github.com/VelinXd/Dv-kernelRMX2189Q kernel && cd kernel
 bash run.sh
 else
-git clone --depth=1 https://github.com/VelinXd/kernel_realme_mt6765Q -b main kernel && cd kernel
+git clone --depth=1 https://github.com/VelinXd/Dv-kernelRMX2189Q -b wifi kernel && cd kernel
 bash run.sh
 fi
